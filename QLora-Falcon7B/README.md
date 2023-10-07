ff9ea1b29102bf53bb5a9ca7139f544a8cf22638
Diego Magdaleno <dmmagdal@ucsc.edu>
2023-10-07 19:53:05 -0400

    Added another tutorial on quantizing and finetuning a falcon 7B model

--- QLora-Falcon7B/README.md
+++ QLora-Falcon7B/README.md
@@ -13,10 +13,12 @@ Description: This is a quick example of finetuning the Falcon 7B model with QLor
 	 - [PeftConfig](https://huggingface.co/docs/peft/main/en/package_reference/config#peft.PeftConfig)
 	 - [Huggingface blog](https://huggingface.co/blog/4bit-transformers-bitsandbytes)
  - tutorial
-	 - [GPT-Nwo Video](https://www.youtube.com/watch?v=NRVaRXDoI3g)
+	 - [GPT-Neo Video](https://www.youtube.com/watch?v=NRVaRXDoI3g)
 	 - [GPT-Neo Collab](https://colab.research.google.com/drive/1Vvju5kOyBsDr7RX_YAvp6ZsSOoSMjhKD?usp=sharing#scrollTo=E0Nl5mWL0k2T)
 	 - [Falcon-7B Video](https://www.youtube.com/watch?v=2PlPqSc3jM0)
 	 - [Falcon-7B Collab](https://colab.research.google.com/drive/1BiQiw31DT7-cDp1-0ySXvvhzqomTdI-o?usp=sharing)
+	 - [Medium article](https://medium.com/@srishtinagu19/quantizing-falcon-7b-instruct-for-running-inference-on-colab-bd97066aa49d) to Quantize Falcon 7B Instruct for running inference on colab
+	 - [Medium article](https://vilsonrodrigues.medium.com/run-your-private-llm-falcon-7b-instruct-with-less-than-6gb-of-gpu-using-4-bit-quantization-ff1d4ffbabcc) to Run your private LLM: Falcon-7B-Instruct with less than 6GB of GPU using 4-bit quantization
  - model
 	 - [GPT-NeoX-20B](https://huggingface.co/EleutherAI/gpt-neox-20b)
 	 - [Falcon-7B](https://huggingface.co/tiiuae/falcon-7b)
--- QLora-Falcon7B/finetune_gpt-neo.py
+++ QLora-Falcon7B/finetune_gpt-neo.py
@@ -1,5 +1,5 @@
-# finetune_falcon.py
-# Load the Falcon 7B model and finetune it with QLora.
+# finetune_gpt-neo.py
+# Load the GPT NeoX 20B model and finetune it with QLora.
 # Windows/MacOS/Linux
 
 
@@ -10,7 +10,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import BitsAndBytesConfig
 # from peft import prepare_model_for_kbit_training
 from peft import LoraConfig, get_peft_model
-from trl import SFTTrainer
 
 
 def main():
@@ -20,9 +19,10 @@ def main():
 
 	# Define model for download and the bitsandbytes config for the
 	# quantization step.
-	model_id = "tiiuae/falcon-7b"				# Note that the target_module specified below in LoraConfig will only work with this model
+	model_id = "EleutherAI/gpt-neox-20b"		# Note that the target_module specified below in LoraConfig will only work with this model
 	bnb_config = BitsAndBytesConfig(
 		load_in_4bit=True,						# enable 4bit quantization by replacing the linear layers with fp4/nf4 layers from bitsandbytes
+		bnb_4bit_use_double_quant=True,			# flag used for nested quantization where quantization constants from the first quantization are quantized again
 		bnb_4bit_quant_type='nf4',				# sets the quantization data type in the bnb.nn.Linear4Bit layers (either fp4 or np4)
 		bnb_4bit_compute_dtype=torch.bfloat16,	# sets the computational type which might be different than the input type
 	) # Note that np4 stands for normal point 4 bit 
@@ -33,23 +33,20 @@ def main():
 		model_id,
 		quantization_config=bnb_config,			# quantizes the model with the above config
 		device_map={"": 0},						# note that you will need GPU to quantize the model.
-		trust_remote_code=True,
 	)
 
 	# We have to apply some preprocessing to the model to prepare it 
 	# for training. For that use the prepare_model_for_kbit_training 
 	# method from PEFT.
-	model.config.use_cache()
+	model.gradient_checkpointing_enable()
 	# model = prepare_model_for_kbit_training(model)
 
 	# Define the Lora config for the finetuning.
 	config = LoraConfig(
-		r=64, 								# the rank of the update matrices. Lower rank results in smaller update matrices with fewer trainable parameters
-		lora_alpha=16, 						# LoRA scaling factor
-		target_modules=[
-			"query_key_value", "dense", "dense_h_to_4h", "dense_4h_to_h"
-		], # the modules (for example, attention blocks) to apply the LoRA update matrices
-		lora_dropout=0.1, 					#
+		r=8, 								# the rank of the update matrices. Lower rank results in smaller update matrices with fewer trainable parameters
+		lora_alpha=32, 						# LoRA scaling factor
+		target_modules=["query_key_value"], # the modules (for example, attention blocks) to apply the LoRA update matrices
+		lora_dropout=0.05, 					#
 		bias="none", 						# specifies if the bias parameters should be trained (can be "none", "all", or "lora_only")
 		task_type="CAUSAL_LM"				# 
 	)
@@ -75,16 +72,15 @@ def main():
 		model=model,							# model
 		train_dataset=data["train"],			# dataset
 		args=transformers.TrainingArguments(	# training args
-			per_device_train_batch_size=4,
+			per_device_train_batch_size=1,
 			gradient_accumulation_steps=4,
-			warmup_ratio=0.03,
-			max_steps=500,
-			max_grad_norm=0.3,
+			warmup_steps=2,
+			max_steps=10,
 			learning_rate=2e-4,
-			logging_steps=10,
-			save_steps=10,
-			output_dir="results",
-			optim="paged_adamw_32bit"
+			fp16=True,
+			logging_steps=1,
+			output_dir="outputs",
+			optim="paged_adamw_8bit"
 		),
 		data_collator=transformers.DataCollatorForLanguageModeling(
 			tokenizer, mlm=False
@@ -99,9 +95,9 @@ def main():
 	###################################################################
 
 	model_to_save = trainer.model.module if hasattr(trainer.model, 'module') else trainer.model  # Take care of distributed/parallel training
-	model_to_save.save_pretrained("results-model")
+	model_to_save.save_pretrained("output-model")
 
-	lora_config = LoraConfig.from_pretrained('results-model')
+	lora_config = LoraConfig.from_pretrained('output-model')
 	model = get_peft_model(model, lora_config)
 
 	###################################################################
