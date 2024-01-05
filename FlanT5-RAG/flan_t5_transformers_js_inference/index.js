4ddf0b82a6a47492e1abb4af3d2140d4aee68bbe
Diego Magdaleno <dmmagdal@ucsc.edu>
2024-01-05 12:29:14 -0800

    Managed to get flan-t5 raw inference script with transformersJS working.

--- FlanT5-RAG/flan_t5_transformers_js_inference/index.js
+++ FlanT5-RAG/flan_t5_transformers_js_inference/index.js
@@ -1,4 +1,5 @@
 // Import modules.
+// import path from 'path';
 import * as readline from 'readline';
 import { AutoModelForSeq2SeqLM, AutoTokenizer } from '@xenova/transformers';
 // import { pipeline } from '@xenova/transformers';
@@ -6,22 +7,42 @@ import { AutoModelForSeq2SeqLM, AutoTokenizer } from '@xenova/transformers';
 
 async function  main () {
     // Initialize tokenizer & model.
-    const model_id = 'dmmagdal/flan-t5-small-onnx';
+    // flan-t5-small
+    //  load time: 1 second
+    //  inference time: 0.25 seconds
+    // flan-t5-base
+    //  load time: 5 seconds
+    //  inference time: 0.5 seconds
+    // flan-t5-large
+    //  load time: 45 - 55 seconds
+    //  inference time: 10 - 15 seconds
+    const model_path = '../../../../flan-t5-small';
+    // const model_path = '../../../../flan-t5-base';
+    // const model_path = '../../../../flan-t5-large';
+
+    // const model_path = '../../../../flan-t5-base-onnx';
+    // const model_id = 'dmmagdal/flan-t5-small-onnx';
     // const model_id = 'dmmagdal/flan-t5-base-onnx';
     // const model_id = 'dmmagdal/flan-t5-large-onnx';
     // const model_id = 'dmmagdal/flan-t5-xl-onnx';
-    const cache_dir = model_id.replace('dmmagdal/', '');
+    // const cache_dir = model_id.replace('dmmagdal/', '');
+    const loadStart = new Date();
     let tokenizer = await AutoTokenizer.from_pretrained(
-        model_id,
+        // model_id,
+        model_path,
         {
-            cache_dir: model_id.replace('dmmagdal/', ''),
+            // cache_dir: model_id.replace('dmmagdal/', ''),
+            // cache_dir: cache_dir,
+            local_files_only: true,
         }
     );
     let model = await AutoModelForSeq2SeqLM.from_pretrained(
-        model_id,
+        // model_id,
+        model_path,
         {
             quantized: false,       // passing in quantized: false means that transformers.js wont look for the quantized onnx model files
-            cache_dir: cache_dir,   // passing in cache_dir value to specify where to save files locally.
+            // cache_dir: cache_dir,   // passing in cache_dir value to specify where to save files locally.
+            local_files_only: true,
         }
     );
     // let generator = await pipeline(
@@ -32,36 +53,53 @@ async function  main () {
     //         cache_dir: cache_dir,
     //     }
     // );   // pipeline abstraction for all-in-one
+    const loadEnd = new Date();
+    const loadTime = loadEnd - loadStart;
+    console.log(`Model load time: ${Math.round(loadTime) / 1000} seconds.`);
+
+    // Initialize the console interface.
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
     // Infinite loop. Prompt the model.
     let input_text = '';
     while (input_text !== 'exit') {
         // Take in the input text.
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-
-        // Take in the input text.
-        rl.question('> ', (input) => {
+        rl.question('> ', async (input) => {
+            // Set the input text to be the input from stdin.
             input_text = input;
-            rl.close();
-        });
-        if (input_text === 'exit') {
-            continue;
-        }
+    
+            if (input_text !== 'exit') {
+                // Tokenize and process the text in the model. Print 
+                // the output.
+                let infStart = new Date();
+                let { input_ids } = await tokenizer(input_text);
+                let outputs = await model.generate(input_ids);
+                let decoded = tokenizer.decode(
+                    outputs[0], 
+                    { skip_special_tokens: true },
+                );
+                let infEnd = new Date();
+                let infTime = infEnd - infStart;
+                console.log(decoded);
+                console.log(`Inference time: ${Math.round(infTime) / 1000} seconds.`);
 
-        // Tokenize and process the text in the model. Print the 
-        // output.
-        let { input_ids } = await tokenizer(input_text);
-        let outputs = await model.generate(input_ids);
-        let decoded = tokenizer.decode(
-            outputs[0], 
-            { skip_special_tokens: true },
-        );
-        console.log(decoded);
-        // let decoded_output = await generator(input_text);
-        // console.log(decoded_output);
+                // Pass the text through the pipeline & print the
+                // output.
+                // let decoded_output = await generator(input_text);
+                // console.log(decoded_output);
+            }
+            else {
+                // Close the interface.
+                rl.close();
+            }
+        });
+
+        // Pause the loop until the user provides input (newline
+        // ENTER).
+        await new Promise(resolve => rl.on('line', resolve));
     }
 
     // Exit the program.
@@ -69,4 +107,4 @@ async function  main () {
 }
 
 
-main()
+main();
